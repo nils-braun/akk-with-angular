68b3548abd3a0e01e55c1c623ab217575d0286de
Nils Braun <nils.braun@kit.edu>
2018-01-08 23:22:27 +0100

    Added a search view set for the artists, dances, labels and songs

--- api/views.py
+++ api/views.py
@@ -1,4 +1,6 @@
 from rest_framework import routers, viewsets, mixins, permissions
+from rest_framework.response import Response
+from rest_framework.serializers import HyperlinkedModelSerializer, CharField
 
 from api import models, serializers
 from api.view_set_factory import registerToRouter
@@ -20,6 +22,46 @@ class CreationUserStorer(viewsets.ModelViewSet):
         serializer.save(last_edit_user=user)
 
 
+class DetailedSongList(mixins.ListModelMixin, viewsets.GenericViewSet):
+    serializer_class = serializers.DetailedSongSerializer
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def get_queryset(self):
+        # TODO: include sorting/filtering
+        request = self.request
+        return models.Song.objects.all().order_by("id")
+
+
+class SearchViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
+    queryset = ""
+
+    def serialize_query(self, model_class, name, query_string):
+        query = model_class.objects.order_by(name)
+
+        if query_string:
+            query = query.filter(**{name + "__contains": query_string})
+
+        query = query.all()[:3]
+
+        class ShortSerializer(HyperlinkedModelSerializer):
+            key = CharField(read_only=True, source=name)
+
+            class Meta:
+                model = model_class
+                fields = ("url", "key",)
+
+        serializer = ShortSerializer(query, many=True, context=self.get_serializer_context())
+        return serializer.data
+
+    def list(self, request, *args, **kwargs):
+        query = request.query_params.get("query", None)
+        data = (self.serialize_query(models.Dance, "name", query) +
+                self.serialize_query(models.Artist, "name", query) +
+                self.serialize_query(models.Label, "name", query) +
+                self.serialize_query(models.Song, "title", query))
+        return Response(data)
+
+
 router = routers.DefaultRouter()
 
 registerToRouter(router, "User", order_column="username")
@@ -30,15 +72,5 @@ registerToRouter(router, "Rating", order_column="id", viewset_class=CreationUser
 registerToRouter(router, "Comment", order_column="id", viewset_class=CreationUserStorer)
 registerToRouter(router, "Song", order_column="title", viewset_class=CreationUserStorer)
 
-
-class DetailedSongList(mixins.ListModelMixin, viewsets.GenericViewSet):
-    serializer_class = serializers.DetailedSongSerializer
-    permission_classes = (permissions.IsAuthenticated,)
-
-    def get_queryset(self):
-        # TODO: include sorting/filtering
-        return models.Song.objects.all().order_by("id")
-
-
-router.register("detailed-songs", DetailedSongList, base_name="detailed-songs")
-
+router.register("query", DetailedSongList, base_name="query")
+router.register("search", SearchViewSet, base_name="search")
