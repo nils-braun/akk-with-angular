22f54a3bd133adbb2d0a5dc3636a3d92d2ad7f82
Nils Braun <nils.braun@kit.edu>
2018-01-28 22:31:36 +0100

    Layout stuff

--- static/angular/src/app/basics/search-bar/search-bar.component.ts
+++ static/angular/src/app/basics/search-bar/search-bar.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {Subject} from 'rxjs/Subject';
+import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
   selector: 'app-search-bar',
@@ -6,10 +8,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search-bar.component.css']
 })
 export class SearchBarComponent implements OnInit {
+  searchTerm$ : Subject<string> = new Subject<string>();
 
-  constructor() { }
+  constructor(private router : Router, private route : ActivatedRoute) {
+    this.searchTerm$.debounceTime(100)
+      .distinctUntilChanged()
+      .forEach(term => this.inputEvent(term)).catch();
+  }
 
   ngOnInit() {
+  }
+
+  inputEvent(searchString) {
+    this.router.navigate(['./'], {queryParams: { query: searchString }, queryParamsHandling: "merge",
+      relativeTo: this.route });
   }
 
 }
